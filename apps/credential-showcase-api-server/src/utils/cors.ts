4f2f7e8f3678e92f314cf0f2fc4d6dce9da95b4c
sanderPostma <spostma@sphereon.com>
2025-03-19 13:36:10 +0100

    chore: cors upgrades

--- apps/credential-showcase-api-server/src/utils/cors.ts
+++ apps/credential-showcase-api-server/src/utils/cors.ts
@@ -2,16 +2,29 @@ import type { CorsOptions } from 'cors'
 
 export const corsOptions: CorsOptions = {
   origin: (requestOrigin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
-    const allowedOrigins = process.env.ALLOW_ORIGINS?.split(',') ?? ['*']
+    const origins = process.env.ALLOW_ORIGINS?.replace(/\s+/g, '').split(/[,;]/).filter(Boolean) ?? ['*']
 
     if (!requestOrigin || process.env.CORS_DISABLED === 'true') return callback(null, true)
-    if (allowedOrigins.indexOf(requestOrigin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.'
-      return callback(new Error(msg), false)
+    if (origins.includes('*') || origins.includes(requestOrigin)) {
+      return callback(null, true)
     }
-    return callback(null, true)
+    const msg = 'The CORS policy for this site does not allow access from the specified Origin.'
+    return callback(new Error(msg), false)
   },
-  methods: process.env.ALLOW_METHODS?.split(',') ?? ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: process.env.ALLOW_HEADERS?.split(',') ?? ['Content-Type', 'Authorization', 'X-Requested-With'],
+  methods: (() => {
+    const methods = process.env.ALLOW_METHODS?.replace(/\s+/g, '').split(/[,;]/).filter(Boolean) ?? ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+    return methods.includes('*') ? '*' : methods
+  })(),
+  allowedHeaders: (() => {
+    const headers = process.env.ALLOW_HEADERS?.replace(/\s+/g, '').split(/[,;]/).filter(Boolean) ?? [
+      'Content-Type',
+      'Authorization',
+      'X-Requested-With',
+      'Cache-Control',
+      'Pragma',
+      'Expires',
+    ]
+    return headers.includes('*') ? '*' : headers
+  })(),
   credentials: process.env.ALLOW_CREDENTIALS === 'true',
 }
